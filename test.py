c05f4d60d3930675569058da0f1e2f1822701a48
circunde <51058933+circunde@users.noreply.github.com>
2019-05-28 20:17:14 -0300

    oi

--- test.py
+++ test.py
@@ -5,16 +5,6 @@ def Contas(usuario, passw):
     try:
         Users = pickle.load(open('test.pkl', 'rb'))
         print(Users)
-
-        '''usr = usuario
-        senha = passw
-
-
-        if usr == '' and senha == '':
-            Users.append('*')
-        else:
-            Users.append({usr: senha})'''
-
         pickle.dump(Users, open('test.pkl', 'wb'))
     except:
         Users = []
